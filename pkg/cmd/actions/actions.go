7580d6d1e1f52a2b048f379ea68f6357e4844a41
vilmibm <vilmibm@github.com>
2021-02-05 15:11:12 -0800

    delist future commands

--- pkg/cmd/actions/actions.go
+++ pkg/cmd/actions/actions.go
@@ -33,24 +33,38 @@ func NewCmdActions(f *cmdutil.Factory) *cobra.Command {
 func actionsRun(opts ActionsOptions) {
 	cs := opts.IO.ColorScheme()
 	fmt.Fprint(opts.IO.Out, heredoc.Docf(`
-		Welcome to GitHub Actions on the command line.
+			Welcome to GitHub Actions on the command line.
 
-		%s
-		gh workflow list:    List workflows in the current repository
-		gh workflow run:     Kick off a workflow run
-		gh workflow init:    Create a new workflow
-		gh workflow check:   Check a workflow file for correctness
+			%s
+			gh run list:    List recent workflow runs
+			gh run view:    View details for a given workflow run
 
-		%s
-		gh run list:    List recent workflow runs
-		gh run view:    View details for a given workflow run
-		gh run watch:   Watch a streaming log for a workflow run
-
-		%s
-		gh job view:   View details for a given job
-		gh job run:    Run a given job within a workflow
-	`,
-		cs.Bold("Working with workflows"),
+			%s
+			gh job view:   View details for a given job
+		`,
 		cs.Bold("Working with runs"),
 		cs.Bold("Working with jobs within runs")))
+	/*
+		fmt.Fprint(opts.IO.Out, heredoc.Docf(`
+			Welcome to GitHub Actions on the command line.
+
+			%s
+			gh workflow list:    List workflows in the current repository
+			gh workflow run:     Kick off a workflow run
+			gh workflow init:    Create a new workflow
+			gh workflow check:   Check a workflow file for correctness
+
+			%s
+			gh run list:    List recent workflow runs
+			gh run view:    View details for a given workflow run
+			gh run watch:   Watch a streaming log for a workflow run
+
+			%s
+			gh job view:   View details for a given job
+			gh job run:    Run a given job within a workflow
+		`,
+			cs.Bold("Working with workflows"),
+			cs.Bold("Working with runs"),
+			cs.Bold("Working with jobs within runs")))
+	*/
 }
